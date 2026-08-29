cb4f542bd47fc8303c0a2d6e4770b8e4ab1e6551
chrehall68 <60240707+chrehall68@users.noreply.github.com>
2026-08-29 18:34:48 +0000

    Format java files

--- project1/src/intermediate/Symtab.java
+++ project1/src/intermediate/Symtab.java
@@ -1,6 +1,6 @@
 /**
  * Symbol table class for a simple interpreter.
- * 
+ *
  * (c) 2026 by Ronald Mak
  * Department of Computer Science
  * San Jose State University
@@ -9,26 +9,26 @@ package intermediate;
 
 import java.util.HashMap;
 
-public class Symtab
-{
-    private HashMap<String, SymtabEntry> contents = new HashMap<>();
-    
-    /**
-     * Make an entry.
-     * @param name the entry's name.
-     */
-    public SymtabEntry enter(String name) 
-    { 
-        SymtabEntry entry = new SymtabEntry(name);
-        contents.put(name, entry);
-        
-        return entry;
-    }
-    
-    /**
-     * Look up an entry.
-     * @param name the entry's name.
-     * @return the entry or null if it's not in the symbol table.
-     */
-    public SymtabEntry lookup(String name) { return contents.get(name); }
+public class Symtab {
+  private HashMap<String, SymtabEntry> contents = new HashMap<>();
+
+  /**
+   * Make an entry.
+   * @param name the entry's name.
+   */
+  public SymtabEntry enter(String name) {
+    SymtabEntry entry = new SymtabEntry(name);
+    contents.put(name, entry);
+
+    return entry;
+  }
+
+  /**
+   * Look up an entry.
+   * @param name the entry's name.
+   * @return the entry or null if it's not in the symbol table.
+   */
+  public SymtabEntry lookup(String name) {
+    return contents.get(name);
+  }
 }
