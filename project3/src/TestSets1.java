32277d8d431d67facb7a781cbf0c8ef34c33871b
chrehall68 <chrehall68@gmail.com>
2026-09-24 09:33:38 -0700

    commit converted java file for sets

---
+++ project3/src/TestSets1.java
@@ -0,0 +1,28 @@
+import java.util.HashSet;
+
+import java.util.HashMap;
+
+public class TestSets1
+{
+
+    private static enum inner2 {A, B, C};
+    private static enum days {MON, TUE, WED, THU, FRI, SAT, SUN};
+
+    private static HashSet<Integer> unnamed1;
+    private static HashSet<days> unnamed2;
+    private static HashSet<days> unnamed3;
+    private static HashSet<Integer> unnamed4;
+    private static HashSet<Integer> unnamed5;
+    private static HashSet<days> unnamed6;
+    private static HashSet<Character> unnamed7;
+    private static HashSet<Integer> named1;
+    private static HashSet<Boolean> named2;
+    private static HashSet<Integer> named3;
+    private static HashSet<inner2> named4;
+    private static HashSet<Integer> named5;
+
+    public static void main(String[] args)
+    {
+        System.out.println("Done!");
+    }
+}
